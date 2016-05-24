39a007d537ee3abff3bb99acee70563e0c21c08a
snower <snower@snower.org>
2016-05-24 18:21:35 +0800

    Token的维护可以用了

--- access_token.go
+++ access_token.go
@@ -2,6 +2,9 @@ package weixin
 
 import (
 	"errors"
+	"github.com/i11cn/go_logger"
+	rest "github.com/i11cn/go_rest_client"
+	"math/rand"
 	"time"
 )
 
@@ -9,17 +12,30 @@ type (
 	WXToken interface {
 		Expired()
 		GetToken() string
+		Close()
+	}
+
+	Tokens struct {
+		AccessToken WXToken
+		JSApiToken  WXToken
+		WXCardToken WXToken
 	}
 
 	token_in_local struct {
 		WXConfig
 		token_type int
+		fn         func(rc *rest.RestClient, app_id, app_secret string, log *logger.Logger) (string, int, error)
 		token      string
+		rc         *rest.RestClient
+		log        *logger.Logger
+		flag       chan int
 	}
 
 	token_in_redis struct {
 		WXConfig
 		token_type int
+		rc         *rest.RestClient
+		log        *logger.Logger
 	}
 
 	token_in_etcd struct {
@@ -27,6 +43,8 @@ type (
 		token_type int
 		token      string
 		last       time.Time
+		rc         *rest.RestClient
+		log        *logger.Logger
 	}
 
 	access_token_response struct {
@@ -50,33 +68,85 @@ const (
 )
 
 func (wx *Weixin) SetTokenSource(t, s int) (ret WXToken) {
+	switch t {
+	case AccessToken, JSApiToken, WXCardToken:
+	default:
+		return
+	}
 	switch s {
 	case Local:
-		ret = &token_in_local{wx.WXConfig, t, ""}
+		ret = (&token_in_local{wx.cfg, t, get_access_token, "", wx.rc, wx.log, make(chan int)}).start()
 	case Redis:
 	case Etcd:
+	default:
+		return nil
 	}
 	return
 }
 
-func (wx *Weixin) get_access_token() (string, int, error) {
-	rc := wx.GetRestClient()
+func get_access_token(rc *rest.RestClient, app_id, app_secret string, log *logger.Logger) (string, int, error) {
 	r := &access_token_response{}
-	if _, err := rc.Get("/token?grant_type=client_credential&appid=%s&secret=%s", r, wx.AppID, wx.AppSecret); err != nil {
+	if _, err := rc.Get("/token?grant_type=client_credential&appid=%s&secret=%s", r, app_id, app_secret); err != nil {
+		log.Error(err.Error())
 		return "", 0, err
 	}
 	if r.ErrCode > 0 {
+		log.Error(r.ErrMsg)
 		return "", 0, errors.New(r.ErrMsg)
 	}
+	log.Trace("获取到了Token:", r.Token, " , 有效时长为:", r.Expire, "秒")
 	return r.Token, r.Expire, nil
 }
 
-func (t *token_in_local) start() {
+func (t *token_in_local) start() *token_in_local {
+	t.log.Trace("启动token的维护工作")
+	fn := func(t *token_in_local) {
+		delay := 0
+		for {
+			select {
+			case i := <-t.flag:
+				if i == 0 {
+					t.log.Log("token维护routing退出")
+					return
+				}
+				t.log.Log("强制更新token")
+				token, exp, err := t.fn(t.rc, t.AppID, t.AppSecret, t.log)
+				if err != nil {
+					t.log.Error(err.Error())
+					t.log.Log("延时10秒钟重新获取Token")
+					delay = 10
+					continue
+				}
+				t.token = token
+				delay = exp*9/10 + rand.Intn(exp/20)
+				t.log.Trace("获取到的Token是:", token, "，过期时长为:", exp)
+				t.log.Trace("延时", delay, "秒执行下一次获取Token的操作")
+			case <-time.After(time.Duration(delay) * time.Second):
+				token, exp, err := t.fn(t.rc, t.AppID, t.AppSecret, t.log)
+				if err != nil {
+					t.log.Log("延时10秒钟重新获取Token")
+					delay = 10
+					continue
+				}
+				t.token = token
+				delay = exp*9/10 + rand.Intn(exp/20)
+				t.log.Trace("获取到的Token是:", token, "，过期时长为:", exp)
+				t.log.Trace("延时", delay, "秒执行下一次获取Token的操作")
+			}
+		}
+	}
+	go fn(t)
+	return t
 }
 
 func (t *token_in_local) Expired() {
+	t.flag <- 1
 }
 
 func (t *token_in_local) GetToken() string {
 	return t.token
+}
+
+func (t *token_in_local) Close() {
+	t.flag <- 0
 }
--- weixin.go
+++ weixin.go
@@ -1,111 +1,13 @@
 package weixin
 
 import (
-	"encoding/xml"
 	"github.com/i11cn/go_logger"
-	"net/http"
+	rest "github.com/i11cn/go_rest_client"
+	"math/rand"
 	"time"
 )
 
 type (
-	WXRequestInfo struct {
-		ToUserName   string
-		FromUserName string
-		CreateTime   time.Duration
-		MsgId        int64
-	}
-
-	WXLocationRequest struct {
-		Location_Y float64
-		Location_X float64
-		Scale      int
-		Label      string
-	}
-
-	WXLinkRequest struct {
-		Title       string
-		Description string
-		Url         string
-	}
-
-	WXLocationEvent struct {
-		Longitude float64
-		Latitude  float64
-		Precision float64
-	}
-
-	WXEvent struct {
-		Event    string
-		EventKey string
-		Ticket   string
-		WXLocationEvent
-	}
-
-	WXRequest struct {
-		WXRequestInfo
-		MsgType string
-		Content string
-		WXLocationRequest
-		WXLinkRequest
-		MediaId      string
-		PicUrl       string
-		Format       string
-		ThumbMediaId string
-		WXEvent
-	}
-
-	WXResponseInfo struct {
-		XMLName      xml.Name `xml:"xml"`
-		ToUserName   string
-		FromUserName string
-		CreateTime   int64
-		MsgType      string
-	}
-
-	WXTextResponse struct {
-		WXResponseInfo
-		Content string
-	}
-
-	WXImageResponse struct {
-		WXResponseInfo
-		MediaId string `xml:"Image>MediaId"`
-	}
-
-	WXVoiceResponse struct {
-		WXResponseInfo
-		MediaId string `xml:"Voice>MediaId"`
-	}
-
-	WXVideoResponse struct {
-		WXResponseInfo
-		MediaId     string `xml:"Video>MediaId"`
-		Title       string `xml:"Video>Title"`
-		Description string `xml:"Video>Description"`
-	}
-
-	WXMusicResponse struct {
-		WXResponseInfo
-		Title        string `xml:"Music>Title"`
-		Description  string `xml:"Music>Description"`
-		MusicUrl     string `xml:"Music>MusicUrl"`
-		HQMusicUrl   string `xml:"Music>HQMusicUrl"`
-		ThumbMediaId string `xml:"Music>ThumbMediaId"`
-	}
-
-	WXNewsItem struct {
-		Title       string
-		Description string
-		PicUrl      string
-		Url         string
-	}
-
-	WXNewsResponse struct {
-		WXResponseInfo
-		ArticleCount int
-		Items        []WXNewsItem `xml:"Articles>item"`
-	}
-
 	WXConfig struct {
 		Token          string
 		EncodingAESKey string
@@ -119,116 +21,53 @@ type (
 	WXService struct {
 	}
 
-	WeiXin2 struct {
+	Weixin struct {
 		cfg     WXConfig
-		tokens  Tokens2
+		tokens  Tokens
 		service *WXService
 		msg     *WXMessage
+		rc      *rest.RestClient
+		log     *logger.Logger
 	}
 )
 
-type (
-	OnValidateFail     func(w http.ResponseWriter, r *http.Request)
-	UnsupportedRequest func(w http.ResponseWriter, info *WXRequestInfo)
-	OnRequestError     func(w http.ResponseWriter, r *http.Request)
-
-	OnTextRequest     func(w http.ResponseWriter, info *WXRequestInfo, content string)
-	OnLocationRequest func(w http.ResponseWriter, info *WXRequestInfo, pos *WXLocationRequest)
-	OnImageRequest    func(w http.ResponseWriter, info *WXRequestInfo, mid, url string)
-	OnVoiceRequest    func(w http.ResponseWriter, info *WXRequestInfo, mid, format string)
-	OnVideoRequest    func(w http.ResponseWriter, info *WXRequestInfo, mid, thumb string, short bool)
-	OnLinkRequest     func(w http.ResponseWriter, info *WXRequestInfo, req *WXLinkRequest)
-
-	OnSubscribeEvent func(w http.ResponseWriter, info *WXRequestInfo, sub bool)
-	OnQRScanEvent    func(w http.ResponseWriter, info *WXRequestInfo, key, ticket string)
-	OnLocationEvent  func(w http.ResponseWriter, info *WXRequestInfo, pos *WXLocationEvent)
-	OnMenuEvent      func(w http.ResponseWriter, info *WXRequestInfo, key string)
-	OnLinkEvent      func(w http.ResponseWriter, info *WXRequestInfo, url string)
+var (
+	g_log *logger.Logger = logger.GetLogger("weixin")
 )
 
-func (info *WXRequestInfo) Response(w http.ResponseWriter, v interface{}) {
-	output, err := xml.MarshalIndent(v, "", "")
-	if err != nil {
-		logger.GetLogger("weixin").Error("创建响应xml失败:", err.Error())
-		w.WriteHeader(500)
-		return
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	g_log.AddAppender(logger.NewConsoleAppender("[%N] %L (%f) : %M"))
+	//g_log.AddAppender(logger.NewSplittedFileAppender("[%N] %L (%f) : %M", "weixin.log", 24*time.Hour))
+}
+
+func NewWeixin(cfg WXConfig) *Weixin {
+	ret := &Weixin{cfg: cfg, tokens: Tokens{}, rc: rest.NewClient("api.weixin.qq.com", 0, "/cgi-bin"), log: g_log}
+	ret.rc.SSL = true
+	ret.tokens.AccessToken = ret.SetTokenSource(AccessToken, Local)
+	return ret
+}
+
+func (wx *Weixin) SetLogger(log *logger.Logger) *Weixin {
+	wx.log = log
+	return wx
+}
+
+func (wx *Weixin) GetAccessToken() string {
+	if wx.tokens.AccessToken != nil {
+		return wx.tokens.AccessToken.GetToken()
 	}
-	w.Write(output)
-	logger.GetLogger("weixin").Trace("给用户返回响应:", string(output))
+	return ""
 }
-
-func (info *WXRequestInfo) ResponseText(w http.ResponseWriter, content string) {
-	resp := WXTextResponse{}
-	resp.ToUserName = info.FromUserName
-	resp.FromUserName = info.ToUserName
-	resp.CreateTime = time.Now().Unix()
-	resp.MsgType = "text"
-	resp.Content = content
-	info.Response(w, resp)
-}
-
-func (info *WXRequestInfo) ResponseImage(w http.ResponseWriter, id string) {
-	resp := WXImageResponse{}
-	resp.ToUserName = info.FromUserName
-	resp.FromUserName = info.ToUserName
-	resp.CreateTime = time.Now().Unix()
-	resp.MsgType = "image"
-	resp.MediaId = id
-	info.Response(w, resp)
-}
-
-func (info *WXRequestInfo) ResponseVoice(w http.ResponseWriter, id string) {
-	resp := WXVoiceResponse{}
-	resp.ToUserName = info.FromUserName
-	resp.FromUserName = info.ToUserName
-	resp.CreateTime = time.Now().Unix()
-	resp.MsgType = "voice"
-	resp.MediaId = id
-	info.Response(w, resp)
-}
-
-func (info *WXRequestInfo) ResponseVideo(w http.ResponseWriter, id, title, desc string) {
-	resp := WXVideoResponse{}
-	resp.ToUserName = info.FromUserName
-	resp.FromUserName = info.ToUserName
-	resp.CreateTime = time.Now().Unix()
-	resp.MsgType = "video"
-	resp.MediaId = id
-	resp.Title = title
-	resp.Description = desc
-	info.Response(w, resp)
-}
-
-func (info *WXRequestInfo) ResponseMusic(w http.ResponseWriter, id, title, desc, url, hqurl string) {
-	resp := WXMusicResponse{}
-	resp.ToUserName = info.FromUserName
-	resp.FromUserName = info.ToUserName
-	resp.CreateTime = time.Now().Unix()
-	resp.MsgType = "music"
-	resp.ThumbMediaId = id
-	resp.Title = title
-	resp.Description = desc
-	resp.MusicUrl = url
-	resp.HQMusicUrl = hqurl
-	info.Response(w, resp)
-}
-
-func (info *WXRequestInfo) ResponseNews(w http.ResponseWriter, articles []WXNewsItem) {
-	resp := WXNewsResponse{}
-	resp.ToUserName = info.FromUserName
-	resp.FromUserName = info.ToUserName
-	resp.CreateTime = time.Now().Unix()
-	resp.MsgType = "news"
-	resp.ArticleCount = len(articles)
-	resp.Items = articles
-	info.Response(w, resp)
-}
-
-func NewWeixinServ(conf *WXConfig, uc interface{}) *Weixin {
-	serv := &Weixin{WXConfig: *conf, user_custom: uc}
-	if serv.init() {
-		return serv
-	} else {
-		return nil
+func (wx *Weixin) GetJSApiToken() string {
+	if wx.tokens.JSApiToken != nil {
+		return wx.tokens.JSApiToken.GetToken()
 	}
+	return ""
+}
+func (wx *Weixin) GetWXCardToken() string {
+	if wx.tokens.WXCardToken != nil {
+		return wx.tokens.WXCardToken.GetToken()
+	}
+	return ""
 }
