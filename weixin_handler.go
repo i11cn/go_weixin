883462a82856bd5ccd8335c5ea8faab49f786135
snower <snower@snower.org>
2016-05-25 18:42:36 +0800

    统一校验签名

--- weixin_handler.go
+++ weixin_handler.go
@@ -26,6 +26,17 @@ func NewHandler(cfg WXConfig, tk WXToken, log *logger.Logger) (*WXHandler, error
 
 func (wh *WXHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	wh.log.Trace("收到一次", r.Method, "请求 : ", r.URL)
+	v := r.URL.Query()
+	if err := exist_all_values(v, "signature", "timestamp", "nonce"); err != nil {
+		wh.log.Error("没有校验的签名，请求不是来自微信")
+		w.WriteHeader(500)
+		return
+	}
+	if !check_sign(wh.cfg.Token, v.Get("signature"), v.Get("timestamp"), v.Get("nonce"), wh.log) {
+		wh.log.Error("签名验证失败")
+		w.WriteHeader(301)
+		return
+	}
 	switch strings.ToUpper(r.Method) {
 	case "GET":
 		wh.doGet(w, r)
@@ -37,17 +48,11 @@ func (wh *WXHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (wh *WXHandler) doGet(w http.ResponseWriter, r *http.Request) {
-	v := r.URL.Query()
-	if err := exist_all_values(v, "signature", "timestamp", "nonce", "echostr"); err != nil {
-		wh.log.Log("目前好像只有验证服务器的请求是GET方法，怎么有个这么乱的请求？")
-		w.WriteHeader(500)
-		return
-	}
-	if check_sign(wh.cfg.Token, v.Get("signature"), v.Get("timestamp"), v.Get("nonce"), wh.log) {
-		w.Write([]byte(v.Get("echostr")))
+	if _, ok := r.URL.Query()["echostr"]; ok {
+		w.Write([]byte(r.URL.Query().Get("echostr")))
 	} else {
-		wh.log.Error("签名验证失败")
-		w.WriteHeader(301)
+		wh.log.Error("错误的请求，没有echostr")
+		w.WriteHeader(500)
 	}
 }
 
